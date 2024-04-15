b8dbd8aa81921f6527f6c03599417ee33fa1c9c2
Max Kottong <max.kottong@gmail.com>
2024-04-14 22:52:47 -0400

    Fixed 2nd page of mobile homepage

--- assets/scripts/script.js
+++
@@ -1,9 +0,0 @@
-const hidden = document.querySelector('.hidden');
-
-window.addEventListener("resize", resize);
-
-function resize() {
-    if (window.innerWidth > 576) {
-        hidden.classList.remove('hidden');
-    }
-}
